ae7512e032045f04bb4779d6b6f62836f0bef26d
fmitei <faith.mitei@trustgro.com>
2023-02-16 16:16:10 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogBreed, setDogBreed] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogBreed(data.message);
+        console.log("data here ..", data.message);
+        setIsLoading(true);
+      });
+  }, []);
+
+  if (!isLoading) return <p>Loading...</p>;
+  return (
+    <div>
+      <img src={dogBreed} alt="A random dog" />
+    </div>
+  );
+}
+
+export default App;
